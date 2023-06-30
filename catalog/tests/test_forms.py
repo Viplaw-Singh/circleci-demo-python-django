110cc123e9df9dd0bc433de2ea3dc89670540357
Viplaw-Singh <134389086+Viplaw-Singh@users.noreply.github.com>
2023-06-30 11:05:53 +0530

    Update test_forms.py

--- catalog/tests/test_forms.py
+++ catalog/tests/test_forms.py
@@ -8,7 +8,7 @@ import xmlrunner
 from django.utils import timezone
 from catalog.forms import RenewBookForm
 from catalog.tests import test_forms
-from catalog.tests import test_models
+from catalog.tests.test_models import AuthorModelTest
 from catalog.tests import test_views
 
 class RenewBookFormTest(TestCase):
